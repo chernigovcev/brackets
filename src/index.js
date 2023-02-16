080038a1f4f57ca038769fe26cd6bffdc1d042d4
Dmitry Chernigovcev <100066868+chernigovcev@users.noreply.github.com>
2023-02-16 17:23:34 +0000

    init: brackets task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const pairs = new Map(bracketsConfig);
+  const stack = [];
+  for (const char of str) {
+    if (pairs.get(stack.at(-1)) == char) {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+  }
+  return stack.length == 0;
+
 }
